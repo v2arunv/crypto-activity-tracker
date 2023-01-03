8e7f8bed5da30aabc374900d44ffa10217bc52ad
Varun Vasudevan <varunv@duck.com>
2023-01-03 20:10:56 +0530

    Update protobufs

--- scraper/rpc/__init__.py
+++ scraper/rpc/__init__.py
@@ -1,7 +1,7 @@
 # Must have code in order to make sure that the generate *_grpc.py 
 # code can access the generated *_pb2.py code. This does unfortunately
-# mean that we're injecting another path into the import path, but
-# it's the only way I've found to make this work.
+# mean that we're injecting another path into the global set of import paths,
+# but it's the only way I've found to make this work.
 import sys, os
 sys.path.append(os.path.dirname(__file__))
 
